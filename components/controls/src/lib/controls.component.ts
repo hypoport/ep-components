1abc7da0be96ac96788b298f3b3cae5dfafde803
Marc Redemske <marc.redemske@gmail.com>
2020-07-25 13:16:18 +0200

    add controls lib

---
+++ components/controls/src/lib/controls.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'ep-controls',
+  template: `
+    <p>
+      controls works!
+    </p>
+  `,
+  styles: [
+  ]
+})
+export class ControlsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ components/controls/src/lib/controls.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from '@angular/core';
+import { ControlsComponent } from './controls.component';
+
+
+
+@NgModule({
+  declarations: [ControlsComponent],
+  imports: [
+  ],
+  exports: [ControlsComponent]
+})
+export class ControlsModule { }
